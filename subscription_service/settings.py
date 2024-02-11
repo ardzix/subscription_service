dd96e2ff0579192a1c20a75225ba423b02a1d7a6
Arif Dzikrullah <ardzyix@gmail.com>
2024-02-11 17:34:38 +0700

    feat: static files use s3

--- subscription_service/settings.py
+++ subscription_service/settings.py
@@ -161,3 +161,13 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Base URL
 BASE_URL = os.getenv('BASE_URL')
+
+# S3 Configuration
+USE_S3 = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+AWS_S3_SIGNATURE_VERSION = "s3v4"
